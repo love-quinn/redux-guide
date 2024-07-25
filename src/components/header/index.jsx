9e9293bb18f718963fa144665b5a091f66ca5705
BrazilianBeast <lucaslucal75@gmail.com>
2024-07-25 14:50:34 -0300

    feat: add user reducer

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 // Components
 import Cart from "../cart/index";
@@ -6,24 +7,41 @@ import Cart from "../cart/index";
 // Styles
 import * as Styles from "./styles";
 
+import {loginUser, logoutUser} from '../../redux/user/actions';
+
 function Header() {
-  const [cartIsVisible, setCartIsVisible] = useState(false);
+    const [cartIsVisible, setCartIsVisible] = useState(false);
 
-  const handleCartClick = () => {
-    setCartIsVisible(true);
-  };
+    const { currentUser } = useSelector((rootReducer) => rootReducer.userReducer);
+    const dispatch = useDispatch();
 
-  return (
-    <Styles.Container>
-      <Styles.Logo>Redux Shopping</Styles.Logo>
-      <Styles.Buttons>
-        <div>Login</div>
-        <div onClick={handleCartClick}>Carrinho</div>
-      </Styles.Buttons>
+    const handleCartClick = () => {
+        setCartIsVisible(true);
+    };
 
-      <Cart isVisible={cartIsVisible} setIsVisible={setCartIsVisible} />
-    </Styles.Container>
-  );
+    const handleLoginClick = ()=> {
+        dispatch(loginUser({name: "Lucas", email:"lucas@email.com"}));
+    };
+
+    const handleLogoutClick = ()  => {
+        dispatch(logoutUser());
+    }
+
+    return (
+        <Styles.Container>
+            <Styles.Logo>Redux Shopping</Styles.Logo>
+            <Styles.Buttons>
+                {currentUser ? (
+                    <div onClick={handleLogoutClick}>Sair</div>
+                ): (
+                    <div onClick={handleLoginClick}>Login</div>
+                )}
+                <div onClick={handleCartClick}>Carrinho</div>
+            </Styles.Buttons>
+
+            <Cart isVisible={cartIsVisible} setIsVisible={setCartIsVisible} />
+        </Styles.Container>
+    );
 }
 
 export default Header;
